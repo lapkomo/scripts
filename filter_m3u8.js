7fc2da8429063c10ec57b92c7a67b2802c6d9d43
lapkomo <lapkomo+dev@gmail.com>
2025-06-14 13:19:54 +0900

    add more

--- filter_m3u8.js
+++ filter_m3u8.js
@@ -4,17 +4,31 @@
 // @match       https://myfans.jp/posts/*
 // @match       https://fod.fujitv.co.jp/title/*
 // @match       https://tver.jp/episodes/*
-// @grant       none
+// @match       https://candfans.jp/posts/*
+// @require   https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js
+// @require   https://cdn.jsdelivr.net/npm/jquery-ui@1/dist/jquery-ui.min.js
+// @resource  jquery_ui_css https://cdn.jsdelivr.net/npm/jquery-ui@1/themes/base/jquery-ui.min.css
+// @grant     GM.getResourceText
+// @grant     GM.addStyle
 // ==/UserScript==
 
+// URLの最後のパスを取得する
+const path = window.location.pathname;
+
+// パスを "/" で分割して最後の部分を取得する
+const lastPathSegment = path.substring(path.lastIndexOf('/') + 1);
+
 (function(open) {
+  $(".hlsfill").remove()
+  $("body").prepend('<div class="hlsfill"></div>')
+  const command = `ffmpeg -movflags faststart -c copy -bsf:a aac_adtstoasc ${lastPathSegment}.mp4 -analyzeduration 50M -probesize 50M -i `
     XMLHttpRequest.prototype.open = function() {
-        // console.log(arguments);
         // arguments[1] がリクエストのURLにあたる (通常、arguments[0] はHTTPメソッド)
-        const url = arguments[1];
+        const url = '"' + arguments[1] + '"';
         // URL に '.m3u8' が含まれている場合のみログに表示
         if (url && url.includes('.m3u8')) {
-            console.log(url);
+          const tag = '<code>' + command + url + '</code><br />'
+            $(".hlsfill").prepend(tag)
         }
         open.apply(this, arguments);
     };
--- togetter_simple_mode.js
+++ togetter_simple_mode.js
@@ -1,7 +1,8 @@
 // ==UserScript==
-// @name     togetter 簡易モード
-// @include  https://togetter.com/*
-// @require  https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js
+// @name      togetter 簡易モード
+// @include   https://togetter.com/*
+// @include   https://posfie.com/*
+// @require   https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js
 // @require   https://cdn.jsdelivr.net/npm/jquery-ui@1/dist/jquery-ui.min.js
 // @resource  jquery_ui_css https://cdn.jsdelivr.net/npm/jquery-ui@1/themes/base/jquery-ui.min.css
 // @grant     GM.getResourceText
@@ -13,6 +14,16 @@ $("#subheader").append('<li><a id="simple_mode" class="light-btn" href="#">簡�
 $("#simple_mode").on("click", function( event ) {
 	simple_mode();
 } );
+
+document.addEventListener('keydown', event => {
+  if (event.code === 'IntlRo') {
+    simple_mode();
+  } else if (event.code === 'ArrowLeft') {
+    $('.pagenation a:contains("前へ")')[0].click();
+  } else if (event.code === 'ArrowRight') {
+    $('.pagenation a:contains("次へ")')[0].click();
+  }
+});
 
 function simple_mode(){
   $("#fixed_header").remove();
@@ -29,7 +40,7 @@ function simple_mode(){
 	$("aside").remove();
   $("footer").remove();
 
-  
+
 	$("div").removeClass("left");
 
 	$(".list_box .list_photo_box").css('flex-flow','row wrap');
